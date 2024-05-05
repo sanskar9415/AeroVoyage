ce46c65950e0591b18642a45f7de9408e2f23b69
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-05-05 13:33:47 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -3,9 +3,13 @@ const {City} = require('../models/index');
 class CityRepository{
     async createCity({ name }){
         try{
-            const city = await City.create({ name });
+            const city = await City.create({ 
+                name
+                //above line equivalent to name:name (its just a shorthand);
+             });
             return city;
         } catch (error){
+            console.log("Something went wrong in the Repository layer");
             throw {error};
         }
     }
@@ -18,8 +22,34 @@ class CityRepository{
                     id: cityId
                 }
             });
-            
+            return true;
         } catch (error){
+            console.log("Something went wrong in the Repository layer");
+            throw {error};
+        }
+    }
+
+    async updateCity({cityId} , data){
+        try {
+            const city = await City.update(data , {
+                where: {
+                    id: cityId
+                }
+            });
+            return city;
+            
+        } catch (error) {
+            console.log("Something went wrong in the Repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity({cityId}){
+        try {
+             const city = await City.findByPk(cityId);
+             return city;
+        } catch (error) {
+            console.log("Something went wrong in the Repository layer");
             throw {error};
         }
     }
