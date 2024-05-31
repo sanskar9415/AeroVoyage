7b3d2620fbeaea8a448c43db82290ded47145aae
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-05-31 17:23:07 +0530

    Added crud classes for crud opertions and used this crud for airport

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,6 +1,11 @@
+const AirportRepository = require('./airport-repository');
+const CrudRespository = require('./crud-repository');
+
 // const CityRepository = require("./city-repository"); or we can do this
 module.exports = {
     CityRepository: require('./city-repository'),
     FlightRepository: require('./flight-repository'),
-    AirplaneRepository: require('./airplane-repository')
+    AirplaneRepository: require('./airplane-repository'),
+    AirportRepository: require('./airport-repository'),
+    CrudRespository: require('./crud-repository')
 }
--- src/services/index.js
+++ src/services/index.js
@@ -2,5 +2,7 @@ const FlightService = require("./flight-service");
 
 module.exports = {
     CityService: require("./city-service"),
-    FlightService: require('./flight-service')
+    FlightService: require('./flight-service'),
+    AirportService: require('./airport-service'),
+    CrudService: require('./crud-service')
 }
