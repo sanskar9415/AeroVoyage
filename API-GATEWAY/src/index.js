b4fae32af947b5efe1fbb4425b1ad3abf641f8bf
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-06-07 14:09:06 +0530

    password encryption done and route setup

--- API-GATEWAY/src/index.js
+++ API-GATEWAY/src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig.js');
+const apiRoutes = require('./routes/index.js')
+
 const app = express();
 
-console.log(PORT)
-
 const PrepareAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended : true}));
+
+    app.use('/api' , apiRoutes);
+
     app.listen(PORT , ()=>{
         console.log(`server started at port : ${PORT}`);
     })
