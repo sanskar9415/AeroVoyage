3f972dfd33aef5da433fcc04c1dee3faf7aed481
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-06-07 12:38:05 +0530

    add crud for user for signup

---
+++ API-GATEWAY/src/repository/user-repository.js
@@ -0,0 +1,34 @@
+const { User } = require('../models/index')
+
+class UserRepository {
+
+    async create(data){
+        try {
+            const user = await User.create(data);
+            return user;
+        } catch (error) {
+            console.log('something went wrong at the repository layer');
+            throw error;
+        }
+    }
+
+
+    async destroy(userId){
+        try {
+            await User.destroy({
+                where: {
+                    id: userId,
+                }
+            });
+        } catch (error) {
+            console.log('something went wrong at the repository layer');
+            throw error;
+        }
+    }
+
+
+
+}
+
+
+module.exports = UserRepository;
