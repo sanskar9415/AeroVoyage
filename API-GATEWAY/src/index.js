cfd0adfc2114ad394c645d92f85958f19b60415c
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-06-07 17:16:09 +0530

    Added get user by id in repo

--- API-GATEWAY/src/index.js
+++ API-GATEWAY/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig.js');
 const apiRoutes = require('./routes/index.js')
+const UserRepository = require('./repository/user-repository.js')
 
 const app = express();
 
@@ -12,9 +13,15 @@ const PrepareAndStartServer = () => {
 
     app.use('/api' , apiRoutes);
 
-    app.listen(PORT , ()=>{
+    app.listen(PORT , async ()=>{
         console.log(`server started at port : ${PORT}`);
+        // const userRepository = new UserRepository();
+
+        // const user = await userRepository.getById(3);
+    
+        // console.log(user);
     })
+
 }
 
 PrepareAndStartServer();
--- API-GATEWAY/src/repository/user-repository.js
+++ API-GATEWAY/src/repository/user-repository.js
@@ -26,7 +26,17 @@ class UserRepository {
         }
     }
 
-
+    async getById(userId){
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: ['email' , 'id']
+            });
+            return user;
+        } catch (error) {
+            console.log('something went wrong at the repository layer');
+            throw error;
+        }
+    }
 
 }
 
