9f0a5246db11f3a8ea21b2e25bae281178ebade7
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-06-17 13:38:27 +0530

    sequelize and routes setup

--- BookingService/src/index.js
+++ BookingService/src/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index.js');
+const db = require('./models/index.js')
 const app = express();
 
-const PORT = 3002;
 
 const setUpAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.use('/api' , apiRoutes);
+
     app.listen(PORT , () => {
-
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({extended: true}));
-
-
         console.log(`Server started on port ${PORT}`);
+
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter: true});
+        }
     });
 }
 setUpAndStartServer();
