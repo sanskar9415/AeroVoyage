65fae97f793e1f9517224d9ed5a4d2a905b9ad55
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-06-23 02:19:15 +0530

    setup basic server for reminder service

---
+++ ReminderService/src/config/serverConfig.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+module.exports = {
+    PORT : process.env.PORT,
+    FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH
+}
---
+++ ReminderService/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { PORT } = require('./config/serverConfig');
+
+const app = express();
+
+
+const setUpAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+
+    app.listen(PORT , () => {
+        console.log(`Server started on port ${PORT}`);
+    });
+}
+setUpAndStartServer();
