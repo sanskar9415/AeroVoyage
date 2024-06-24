c8c19aaf9e3bce8728484372f485e768b9a18757
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-06-24 14:07:00 +0530

    setup smtp using nodemailer done

--- ReminderService/src/config/serverConfig.js
+++ ReminderService/src/config/serverConfig.js
@@ -3,5 +3,7 @@ dotenv.config();
 
 module.exports = {
     PORT : process.env.PORT,
-    FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH
+    FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH,
+    EMAIL_ID: process.env.EMAIL_ID,
+    EMAIL_PASS: process.env.EMAIL_PASS
 }
--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
+const { sendBasicEmail } = require('./services/email-service');
 
 const app = express();
 
@@ -12,6 +13,13 @@ const setUpAndStartServer = () => {
 
     app.listen(PORT , () => {
         console.log(`Server started on port ${PORT}`);
+
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'sanskarrrgupta@gmail.com',
+        //     'This is a testing email',
+        //     'helloww from my reminder service'
+        // )
     });
 }
 setUpAndStartServer();
