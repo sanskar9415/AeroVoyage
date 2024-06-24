9de5f40665f0997b04106ddd932bd21da8d1d6e6
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-06-24 22:27:20 +0530

    setup basic cron

--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
-const { sendBasicEmail } = require('./services/email-service');
+// const { sendBasicEmail } = require('./services/email-service');
+const cron = require('node-cron');
 
 const app = express();
 
@@ -13,6 +14,10 @@ const setUpAndStartServer = () => {
 
     app.listen(PORT , () => {
         console.log(`Server started on port ${PORT}`);
+
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every 2 minutes');
+        });
 
         // sendBasicEmail(
         //     'support@admin.com',
