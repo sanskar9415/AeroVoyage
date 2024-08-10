3008440c5fdf6b20525783bd61e3ea3161fc35b1
Sanskar Gupta <sanskargupta9415@gmail.com>
2024-08-10 22:41:59 +0530

    Rabbit MQ setup

--- ReminderService/src/config/serverConfig.js
+++ ReminderService/src/config/serverConfig.js
@@ -5,5 +5,8 @@ module.exports = {
     PORT : process.env.PORT,
     FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH,
     EMAIL_ID: process.env.EMAIL_ID,
-    EMAIL_PASS: process.env.EMAIL_PASS
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
+    EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+    REMINDER_BINDING_KEY: process.env.REMINDER_BINDING_KEY
 }
--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -5,14 +5,17 @@ const { PORT } = require('./config/serverConfig');
 const TicketController = require('./controllers/ticket-controller');   
 
 const jobs = require('./utils/job');
+const { createChannel } = require('./utils/messageQueue');
 
 const app = express();
 
 
-const setUpAndStartServer = () => {
+const setUpAndStartServer = async () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
+    const channel = await createChannel();
 
     app.post('/api/v1/tickets' , TicketController.create);
 
